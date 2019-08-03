ad55d7f2d96a594a3a1f31e88c8a9c9b5723d262
Amy Fogelman <abfnj1@gmail.com>
2019-08-03 13:32:04 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,22 +1,17 @@
-require 'nokogiri'
-require 'open-uri'
-
-class Course
- 
+rclass Course
   attr_accessor :title, :schedule, :description
- 
+
   @@all = []
- 
+
   def initialize
-    @@all << self
+    self.class.all << self
   end
- 
+
   def self.all
     @@all
   end
- 
+
   def self.reset_all
     @@all.clear
   end
- 
 end
