c57c50da6b2ce2017edd9b3723b189aef5935033
Amy Fogelman <abfnj1@gmail.com>
2019-08-03 13:28:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,6 +10,10 @@ class Scraper
  
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
  
+ def get_courses
+  self.get_page.css(".post")
+end
+ 
     #doc.css(".post").each do |post|
       #course = Course.new
       #course.title = post.css("h2").text
